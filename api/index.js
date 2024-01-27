2332616d8a95ca5c290bce6f7657d06ad33f4966
firespittermonster <sansamate@gmail.com>
2024-01-27 20:32:08 +0530

    MongoDB setup

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
+
+const password = process.env.MONGODB_PASSWORD;
+const connectionString = `mongodb+srv://sanved:${encodeURIComponent(password)}@mern-real-estate.hmj1fhr.mongodb.net/MERN-Real-Estate?retryWrites=true&w=majority`;
+
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
 app.listen(3000, () => {
-    console.log('server is running on port 3000');
-}
-    )
+    console.log('Server is running on port 3000');
+});
